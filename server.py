06092701daebe240cd674f7334fc4b2fadbfe49a
davidurpani <davidurpani@users.noreply.github.com>
2020-05-12 21:12:15 +1000

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
+model_file_url = 'https://www.dropbox.com/s/8kdjnok4fw3noap/teddys.pth?raw=1'
 model_file_name = 'model'
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
